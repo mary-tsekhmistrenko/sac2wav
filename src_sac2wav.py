563f65f67cfd603865965ed8532fee4295c81d67
mary-tsekhmistrenko <maria.tsekhmistrenko@gmail.com>
2021-08-10 14:00:46 +0100

    normalize, output naming, chunkwise continuous proc

--- src_sac2wav.py
+++ src_sac2wav.py
@@ -121,13 +121,28 @@ def generate_output_folders(mode, save_path):
 # -----------------------------------------------------------------------
 
 def export_continuous(df, poly_wav, dmt_folder, folder_to_process, proc_wavs_continuous, station_selection, channel_selection,
-                      framerate, bitrate, wav_save, plot_waveforms):
+                      framerate, bitrate, norm, wav_save, plot_waveforms):
 
     fsr = open(os.path.join(wav_save, 'sampling_rate_information.txt'), 'w')
     fsr.write('# station \t|\t channel \t|\t sampling rate \t|\t frame rate \t|\t conversion: 60 min SAC to XX min WAV \n')
 
     uniq_modes = natsorted(df['mode'].unique())
     uniq_sta = df['station'].unique()
+
+    if folder_to_process in 'processed':
+        proc_folder = 'processed'
+        fold_2_proc = 'p'
+    elif folder_to_process in 'proc_resamp':
+         proc_folder = 'proc_resamp'
+         fold_2_proc = 'r'
+    elif folder_to_process in 'proc_instr':
+        proc_folder = 'proc_instr'
+        fold_2_proc = 'i'
+    elif folder_to_process in 'noinstr_noresamp':
+        proc_folder = 'proc_noinstr_noresamp'
+        fold_2_proc = 'ninr'
+    else:
+        sys.exit(f'{folder_tor_process} is not defines. Forced Exit.')
 
     counter = 0
     days_to_proc = []
@@ -136,29 +151,39 @@ def export_continuous(df, poly_wav, dmt_folder, folder_to_process, proc_wavs_con
         pklo = open(os.path.join(folder, 'info', 'event.pkl'), 'rb')
         pkll = pkl.load(pklo)
         cont_date = pkll['datetime']
+
         if cont_date < UTCDateTime(proc_wavs_continuous[0]):
             print(f'{UTCDateTime(proc_wavs_continuous[0])} < {cont_date}: Continue searching for starttime.')
             continue
 
-        if counter >= proc_wavs_continuous[1]:
-            break
-
+        if proc_wavs_continuous[1] == '*':
+            pass
+        else:
+            if counter >= proc_wavs_continuous[1]:
+                break
         cont_folder = os.path.basename(folder)
         days_to_proc.append(cont_folder)
         counter += 1
 
-    print(f'Days to process:\n\t{days_to_proc}')
+    if proc_wavs_continuous[2] == '*':
+        chunk_to_proc = days_to_proc
+        print(f'Days to process:\n\t{chunk_to_proc}')
+        proc_continuous(df, uniq_sta, station_selection, channel_selection,target_cha, uniq_modes, chunk_to_proc, proc_folder, 
+                        framerate, bitrate, norm, poly_wav, fsr, fold_2_proc, dmt_folder, wav_save, plot_waveforms)
 
-    if folder_to_process in 'processed':
-        proc_folder = 'processed'
-    elif folder_to_process in 'proc_resamp':
-         proc_folder = 'proc_resamp'
-    elif folder_to_process in 'proc_instr':
-        proc_folder = 'proc_instr'
-    elif folder_to_process in 'noinstr_noresamp':
-        proc_folder = 'proc_noinstr_noresamp'
     else:
-        sys.exit(f'{folder_tor_process} is not defines. Forced Exit.')
+        chunks_to_proc = list(chunks(days_to_proc, proc_wavs_continuous[2] ))
+        for chunk_to_proc in chunks_to_proc:
+            print(f'Days to process:\n\t{chunk_to_proc}')
+            proc_continuous(df, uniq_sta, station_selection, channel_selection,target_cha, uniq_modes, chunk_to_proc, proc_folder, 
+                            framerate, bitrate, norm, poly_wav, fsr, fold_2_proc, dmt_folder, wav_save, plot_waveforms)
+
+
+    fsr.close()
+
+
+def proc_continuous(df, uniq_sta, station_selection, channel_selection, target_cha, uniq_modes, days_to_proc, proc_folder, 
+                    framerate, bitrate, norm, poly_wav, fsr, fold_2_proc, dmt_folder, wav_save, plot_waveforms):
 
     for i, sta in enumerate(uniq_sta):
         if station_selection != '*' and not sta in station_selection:
@@ -203,13 +228,17 @@ def export_continuous(df, poly_wav, dmt_folder, folder_to_process, proc_wavs_con
             
                     # rest the trace/stream for every channel
                     st = None
+                    
+                    l = 0 
                     for k, mod in enumerate(uniq_modes):
+        
                         if mod not in days_to_proc:
                             continue
                         else:
                             pass
                         
                         print(f'\t\tFolder {mod}')
+                        # import ipdb; ipdb.set_trace()
                         try:
                             chans = glob.glob(os.path.join(dmt_folder, mod, f'{proc_folder}', f'*{sta}*{loc}*{cha}*'))[0]
                             print(f'\t\t\tAdding {os.path.basename(chans)}.')
@@ -219,13 +248,13 @@ def export_continuous(df, poly_wav, dmt_folder, folder_to_process, proc_wavs_con
                             continue
                         
                         # merge to one long trace
-                        if k == 0:
+                        if l == 0:
                             st = read(chans)
-                        if k > 0:
+                        if l > 0:
                             st += read(chans)
+                        l += 1
                         st.merge(method=1)
                     
-                    # here I normalize the data, otherwise writing it out into WAV files clips the waveform
                     if not st:
                         # basically skip the channel
                         continue
@@ -234,11 +263,11 @@ def export_continuous(df, poly_wav, dmt_folder, folder_to_process, proc_wavs_con
                     print(f'\tLength of trace: {tr.stats.npts}')
 
                     # data = tr.data / abs(tr.data).max()
-                    
                     # data = tr.data
                     
                     # order of channels to export needs to be like this: 
                     # ['*HH', '*HY', '*HZ', '*HX']
+                    
                     if tr.stats.channel[-1] in ['E', 'X', '1']:
                         # e_data = data*0.95
                         e_cha = tr.stats.channel
@@ -277,7 +306,6 @@ def export_continuous(df, poly_wav, dmt_folder, folder_to_process, proc_wavs_con
                     fsr.write('\n')
                     # >>> END CHANNEL LOOP <<<<  
 
-
                 if (channel_counter == 3 and h_data == None):
                     print('\t\t\tAdding a zero trace for the hydrophone channel...')
                     h_data = z_tr.stats.npts*[0]
@@ -288,12 +316,14 @@ def export_continuous(df, poly_wav, dmt_folder, folder_to_process, proc_wavs_con
                     
                     channel_counter += 1
                     h_cha = '0DH'
+                    cha_output = f'3{z_tr.stats.channel[0]}'
 
                 elif channel_counter < 3:
                     print(f'\t\t\t{sta} does not have all channels {cha}. Continue to next station without writing a WAV file.')
                     continue 
 
                 elif channel_counter == 4:
+                    cha_output = f'4{z_tr.stats.channel[0]}'
                     pass
 
                 elif (channel_counter == 3 and h_data != None):
@@ -317,18 +347,35 @@ def export_continuous(df, poly_wav, dmt_folder, folder_to_process, proc_wavs_con
                 z_da = st.select(channel=z_cha)
                 e_da = st.select(channel=e_cha)
                 n_da = st.select(channel=n_cha)
-
-                if h_da[0].stats.channel == '0DH':
-                    h_data = h_da[0].data
-                else:
-                    h_data = (h_da[0].data / abs(h_da[0].data).max())*0.95
                 
-                z_data = (z_da[0].data / abs(z_da[0].data).max())*0.95
-                e_data = (e_da[0].data / abs(e_da[0].data).max())*0.95
-                n_data = (n_da[0].data / abs(n_da[0].data).max())*0.95
+                print(f'\tNormalizing after merging continuous waveforms.')
+                print(f'\tMax values of channels HZEN (might be useful for norm factor):', abs(h_da[0].data).max(), abs(z_da[0].data).max(), abs(e_da[0].data).max(), abs(n_da[0].data).max())
 
-                print(f'\tNormalizing channelwise after merging continuous waveforms.')
+                if norm < 0:
+                    if h_da[0].stats.channel == '0DH':
+                        h_data = h_da[0].data
+                    else:
+                        h_data = (h_da[0].data / abs(h_da[0].data).max())*0.95
+                    
+                    norming = max([abs(z_da[0].data).max(), abs(e_da[0].data).max(), abs(n_da[0].data).max()])
 
+                    z_data = (z_da[0].data / norming)*0.95
+                    e_data = (e_da[0].data / norming)*0.95
+                    n_data = (n_da[0].data / norming)*0.95
+                    print(f'\tNorm factor used for three components: {norming}')
+
+                else:
+                    if h_da[0].stats.channel == '0DH':
+                        h_data = h_da[0].data
+                    else:
+                        h_data = (h_da[0].data / norm)
+                    
+                    z_data = (z_da[0].data / norm)
+                    e_data = (e_da[0].data / norm)
+                    n_data = (n_da[0].data / norm)
+                    print(f'\tNorm factor used for all components: {norm}')
+
+                
                 try:
                     collect_tr = np.c_[h_data, n_data, e_data, z_data]
                     collect_cha = [h_cha, n_cha, e_cha, z_cha]
@@ -340,9 +387,9 @@ def export_continuous(df, poly_wav, dmt_folder, folder_to_process, proc_wavs_con
                     plot_waves(sta, collect_tr, collect_cha, tr.stats.sampling_rate, tr.stats.network, tr.stats.location, 'continuous', proc_folder, wav_save)
 
                 if poly_wav:
-                    file_name = (f'{tr.stats.network}.{tr.stats.station}.{tr.stats.location}_{collect_cha[0]}_{collect_cha[1]}_{collect_cha[2]}_{collect_cha[3]}_{folder_to_process}_{tr.stats.starttime.date.year}-{tr.stats.starttime.date.month:02d}-'
-                                f'{tr.stats.starttime.date.day:02d}T{tr.stats.starttime.time.hour:02d}-'
-                                f'{tr.stats.starttime.time.minute:02d}-{tr.stats.starttime.time.second:02d}.WAV')
+                    file_name = (f'{tr.stats.network}.{tr.stats.station}.{tr.stats.location}_{cha_output}_{fold_2_proc}_{st[0].stats.starttime.date.year}-{st[0].stats.starttime.date.month:02d}-'
+                                f'{st[0].stats.starttime.date.day:02d}_{st[0].stats.endtime.date.year}-'
+                                f'{st[0].stats.endtime.date.month:02d}-{st[0].stats.endtime.date.day:02d}.WAV')
 
                     path_file_wav = os.path.join(wav_save, file_name)
                     with SoundFile(path_file_wav, 'w', samplerate=framerate, channels=4, 
@@ -357,9 +404,10 @@ def export_continuous(df, poly_wav, dmt_folder, folder_to_process, proc_wavs_con
                                     subtype=bitrate, endian=None, format=None, closefd=True) as f:
                             f.write(collect_tr[:,j])
                     f.close() 
-    # clear memory
-    del collect_tr, h_da, z_da, e_da, n_da
-    fsr.close()
+                
+                # clear memory after the channels have been written
+                print ('Clearing memeroy...')
+                del collect_tr, h_da, z_da, e_da, n_da
 
 
 # -----------------------------------------------------------------------
@@ -855,6 +903,14 @@ def length_checker(st):
         return st
     else:
         return st
+
+
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
 
 
 # -----------------------------------------------------------------------
